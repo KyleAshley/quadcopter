b80dfe576c1602489ddfb3a648bcf2f995338e9f
Kyle Ashley <kyle.ashley183@gmail.com>
2014-01-10 11:02:32 -0500

    Update accel.h

--- accel.h
+++ accel.h
@@ -1,52 +1,37 @@
 /********************************************/
 // Author: Kyle Ashley
 // accel.h
-// for HCS12 and ADXL345
+// for HC9S12 and ADXL035
 /********************************************/
- 
-#define a_ID 0xA6
+#ifndef ACCEL_H_
+#define ACCEL_H_
 
-#define a_THRESH_TAP 0x1D
-#define a_OFSX 0x1E
-#define a_OFSY 0x1F
-#define a_OFSZ 0x20
-#define a_DUR 0x21
-#define a_LATENT 0x22
-#define a_WINDOW 0x23
-#define a_THRESH_ACT 0x24
-#define a_THRESH_INACT 0x25
-#define a_TIME_INACT 0x26
-#define a_ACT_INACT_CTL 0x27
-#define a_THRESH_FF 0x28
-#define a_TIME_FF 0x29
-#define a_TAP_AXES 0x2A
-#define a_ACT_TAP_STATUS 0x2B
-#define a_BW_RATE 0x2C
-#define a_POWER_CTL 0x2D
-#define a_INT_ENABLE 0x2E
-#define a_INT_MAP 0x2F
-#define a_INT_SOURCE 0x30
-#define a_DATA_FORMAT 0x31
+/* Associated Sensor Variables */
+// Raw Accelerometer Readings
+extern float a_xRaw;
+extern float a_yRaw;
+extern float a_zRaw;
 
-#define a_DATAX0 0x32
-#define a_DATAX1 0x33
-#define a_DATAY0 0x34
-#define a_DATAY1 0x35
-#define a_DATAZ0 0x36
-#define a_DATAZ1 0x37
+// Converted Roll and Pitch Angles
+extern float a_roll;
+extern float a_pitch;
 
-#define a_FIFO_CTL 0x38
-#define a_FIFO_STATUS 0x39
+// ACCEL Associated Functions
+/******************************************************************************/
+// sensor initialization
+void a_setup(void);             // initilizes sensor with default range +/-2g
+void a_setRange(int);           // set sensor sensitivity 2/4/8/16g
+
+// sensor reads
+void a_updateXRaw(void);            // Updates raw X value with a new reading
+void a_updateYRaw(void);            // Updates raw Y value with a new reading
+void a_updateZRaw(void);            // Updates raw Z value with a new reading
+
+// converted calculations to be done after raw sensor reads
+void a_updatePitchDeg(void);    // Calculates and updates converted pitch (Deg)
+void a_updateRollDeg(void);     // Calculates and updates converted roll (Deg)
 
 
-void setupAccel(void);
-void setAccelRange(int);
-double getAccelX(void);
-double getAccelY(void);
-double getAccelZ(void);
-void output_accelXYZ(double, double, double);
-double getAccelPitchDeg(double, double);
-double getAccelRollDeg(double, double, double);
-double getAccelPitchRad(double, double);
-double getAccelRollRad(double, double, double);
-double lpFilter(double, double, double);
+//double a_lpFilter(double, double, double);
+
+#endif
