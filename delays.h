0d63aa8572344efa5811fa0f64814c5585e46653
Kyle Ashley <kyle.ashley183@gmail.com>
2014-01-10 11:05:43 -0500

    Update delays.h

--- delays.h
+++ delays.h
@@ -1,12 +1,17 @@
 /****************************************************************************/
+// delays.h
 // Author: Kyle Ashley
 // Description: Delay functions used for HC9S12 uProccessor
 /****************************************************************************/
+#ifndef DELAYS_H_
+#define DELAYS_H_
 
-void LCDDelayTH(unsigned int num);     // ACTUAL =41 ns per 'num + Overhead
+void LCDDelayTH(unsigned int num);      // ACTUAL =41 ns per 'num + Overhead
 void LCDDelayPWEH(unsigned int num);    // ACTUAL = 458 ns per 'num' + Overhead
 void LCDDelayTAS(unsigned int num);     // ACTUAL = 167 ns per 'num' + Overhead
-void LCDDelayDATA(unsigned int num);   // ACTUAL = (40us per 'num' + Overhead) = 40 us
+void LCDDelayDATA(unsigned int num);    // ACTUAL = (40us per 'num' + Overhead) = 40 us
 void LCDDelaySCROLL(unsigned int num);  // 43 ms per 'num' + Overhead
-void LCDuDelay(unsigned int num);    // ACTUAL = (1.33 us per 'num' + Overhead) = 1.33 us
-                                                                 
+void LCDuDelay(unsigned int num);       // ACTUAL = (1.33 us per 'num' + Overhead) = 1.33 us
+void LCDmsDelay(unsigned int num);       // ACTUAL = 1.33 ms per 'num' + Overhead
+
+#endif
