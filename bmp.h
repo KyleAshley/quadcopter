3ae5b70e3cd80c8c061adb451e975c7972a23b50
Kyle Ashley <kyle.ashley183@gmail.com>
2014-01-10 11:04:55 -0500

    Update bmp.h

--- bmp.h
+++ bmp.h
@@ -4,47 +4,46 @@
 // Description: For use with bmp085 barometric pressure
 // sensor with HCS12 microcontroller
 /******************************************************/
+#ifndef BMP_H_
+#define BMP_H_
 
-#define b_ID 0xEE
-
-// calibration regs
-#define b_AC1 0xAA
-#define b_AC2 0xAC
-#define b_AC3 0xAE
-#define b_AC4 0xB0
-#define b_AC5 0xB2
-#define b_AC6 0xB4
-#define b_B1 0xB6
-#define b_B2 0xB8
-#define b_MB 0xBA
-#define b_MC 0xBC
-#define b_MD 0xBE
-#define OSS 0
-
-#define TEMP_MODE 0x2E
-#define PRESSURE_MODE_0 0x34
-#define PRESSURE_MODE_1 0x74
-#define PRESSURE_MODE_2 0xB4
-#define PRESSURE_MODE_3 0xF4
-
-extern short ac1, ac2, ac3;
-extern unsigned short ac4, ac5, ac6;
-extern short b1, b2, mb, mc, md;
+// Calibration variables
+extern short ac1;
+extern short ac2;
+extern short ac3;
+extern unsigned short ac4;
+extern unsigned short ac5;
+extern unsigned short ac6;
+extern short b1;
+extern short b2;
+extern short mb;
+extern short mc;
+extern short md;
 extern short b5;
-extern long uP;
-extern long uT;
 extern long x1, x2, x3, b3, b6;
 extern unsigned long b4, b7;
-extern long p;
-extern long x1, x2;
-extern double temp;
 
-void calibrateBMP(void);
-void setBMPControlMode(unsigned int);
+// Raw Pressure and Raw Temperature Variables
+extern long uP;
+extern long uT;
 
-double getBMPTempC(void);
-long getBMPPressure(void);
+// Converted pressure, temperature and alitude values
+extern long b_pressure;
+extern float b_tempC;
+extern float b_altitude;
 
-long getBMPRawPressure(void);
-long getBMPRawTemperature(void);
-float calcAltitude(long);
+// BMP Associated Functions
+/******************************************************************************/
+void b_setup(void);                     // loads calibration values into variables
+void b_setControlMode(unsigned int);    // sets bmp for pressure/temp reading
+
+// MUST CALL THESE THREE IN ORDER
+void b_updateTempC(void);               // updates tempC with new sensor reading
+void b_updatePressure(void);            // updates pressure with new sensor reading
+void b_updateAltitude(void);              // updates altitude with new sensor reading
+
+// Accesories to be called by updates
+void b_updateRawPressure(void);
+void b_updateRawTemperature(void);
+
+#endif
