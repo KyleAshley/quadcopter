b0c195c72b1a26ce0ad0daf5d3f7213ea0810c15
Kyle Ashley <kyle.ashley183@gmail.com>
2014-01-10 11:14:58 -0500

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -1,11 +1,15 @@
 /***************************************************/
-// I2C Functions
-// Author: Kyle Ashley
+// IIC Functions
+// AUTHOR: Kyle Ashley
+// Description: Enables easy IIC protocol communication
+// on HC9S12 uP and asscoiated devices
 /***************************************************/
+#ifndef I2C_H_
+#define I2C_H_
 
-void openI2C(unsigned char);
-unsigned char I2C_Read_Byte(unsigned char, unsigned char);
-void I2C_Write_Byte(unsigned char, unsigned char, unsigned char);
-long I2C_Read_2Bytes(unsigned char, unsigned char);
-int I2C_Read_3Bytes(unsigned char, unsigned char);
-void I2C_Read_Bytes(unsigned char, unsigned char, int, int*);
+void openI2C(unsigned char);                                        // initializes IIC communication with given slave address
+unsigned char I2C_Read_Byte(unsigned char, unsigned char);          // Reads one byte from given slave and internal address
+void I2C_Write_Byte(unsigned char, unsigned char, unsigned char);   // Writes one byte to given slave and internal address
+void I2C_Read_Bytes(unsigned char, unsigned char, int, int*);       // Places result of multiple IIC reads into given buffer
+
+#endif
