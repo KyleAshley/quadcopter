8a3028907ee1e253aee1ae1bf6b8f880b2a8c850
Kyle Ashley <kyle.ashley183@gmail.com>
2014-01-10 21:17:29 -0500

    Updated Flight Control Algorithm
    Added Accessory Functions

--- flightControl.c
+++ flightControl.c
@@ -15,100 +15,255 @@
 /* Define:
  *     angle_off(ENUM TYPE) - ENUM TYPE 0=PITCH, 1=ROLL, 2=HEADING, 3=ALTITUDE
  *     unsigned int* motorRelationships(ENUM TYPE); // Returns Doubleton (int num_in_array, unsigned int* array)
- *     change_threshold = 
+ *     change_threshold =
  *
- *
- *
+ *      Quad Setup:             0
+ *                              |
+ *                              |
+ *   Forward Heading ^    3 ----X---- 1
+ *                              |
+ *                              |
+ *                              2
  */
 
 #define PITCH     0
 #define ROLL      1
-#define HEADING   2
-#define ALTITUDE  3
+#define ALTITUDE  2
+#define HEADING   3
 
 #define POSITIVE   1
 #define NEGATIVE  -1
 
-int** get_motors(const int TYPE_CORRECTION)
+#define PITCHGAIN   1.5
+#define ROLLGAIN    1.5
+#define ALTITUDEGAIN    1.5
+#define HEADINGGAIN     1.5
+
+#define ALTTHRESH       0.2         // meters
+#define HEADINGTHRESH   2.0         // degrees
+#define PITCHTHRESH     2.0         // degrees
+#define ROLLTHRESH      2.0         // degrees
+
+#define MINDTY 6000
+#define MAXDTY 12000
+
+/*******************************************************************************/
+// Initializes motors on PWM channels 1, 3, 5, and 7. Call this immediately
+// after powerup
+/*******************************************************************************/
+void fc_initialize_motors()
+{
+    PWMCTL = 0xFC;    // all 16 bit channels
+    PWMCLK = 0xFF;    // all scaled clocks
+    PWMPOL = 0xFF;
+    PWMSCLA = 1;
+    PWMSCLB = 1;
+    PWMPRCLK = 0x11;   // PRE = 2
+
+    // channel 1
+    PWMPER0 = 0x4E;    // 20000
+    PWMPER1 = 0x20;
+    PWMDTY0 = 0x17;    // 6000
+    PWMDTY1 = 0x70;
+
+    // channel 3
+    PWMPER2 = 0x4E;    // 20000
+    PWMPER3 = 0x20;
+    PWMDTY2 = 0x17;    // 6000
+    PWMDTY3 = 0x70;
+
+    // channel 5
+    PWMPER4 = 0x4E;    // 20000
+    PWMPER5 = 0x20;
+    PWMDTY4 = 0x17;    // 6000
+    PWMDTY5 = 0x70;
+
+    // channel 7
+    PWMPER6 = 0x4E;    // 20000
+    PWMPER7 = 0x20;
+    PWMDTY6 = 0x17;    // 6000
+    PWMDTY7 = 0x70;
+
+    PWME |= 0xFF;      // enable all channels
+    LCDDelaySCROLL(270);
+}
+
+/*******************************************************************************/
+// Defines motor compensations in each of four cases where sensor readings
+// differ from dersired value, pitch, roll, heading and altitude
+/*******************************************************************************/
+int** fc_get_motors(const int TYPE_CORRECTION)
 {
 	int **operands = malloc(sizeof(int) + sizeof(int*));
 	if (TYPE_CORRECTION == PITCH) {
 		*operands[0] = 2;
 		operands[1] = malloc((sizeof(unsigned int) + sizeof(int)) * *operands[0]);
-		operands[1][0][0] = 0;  // motor
-		operands[1][0][1] = POSITIVE; // arbitrary
-		operands[1][1][0] = 2;  // opposite motor
-		operands[1][1][1] = NEGATIVE; // important that operands[1][0][1] is inverse
+		operands[1][0][0] = 0;              // positive correction of motor 0
+		operands[1][0][1] = POSITIVE;
+		operands[1][1][0] = 2;              // negative correction of motor 2
+		operands[1][1][1] = NEGATIVE;       // important that operands[1][0][1] is inverse
 	} else if (TYPE_CORRECTION == ROLL) {
 		*operands[0] = 2;
 		operands[1] = malloc((sizeof(unsigned int) + sizeof(int)) * *operands[0]);
-		operands[1][0][0] = 1;  // motor
-		operands[1][0][1] = POSITIVE; // arbitrary
-		operands[1][1][0] = 3;  // opposite motor
-		operands[1][1][1] = NEGATIVE; // important that operands[1][0][1] is inverse
+		operands[1][0][0] = 1;              // positive correction of motor 1
+		operands[1][0][1] = POSITIVE;
+		operands[1][1][0] = 3;              // negative correction of motor 3
+		operands[1][1][1] = NEGATIVE;
+    } else if (TYPE_CORRECTION == ALTITUDE) {
+		*operands[0] = 4;
+		operands[1] = malloc((sizeof(unsigned int) + sizeof(int)) * *operands[0]);
+		operands[1][0][0] = 0;              // positive correction of motor 0
+		operands[1][0][1] = POSITIVE;
+		operands[1][1][0] = 2;              // positive correction of motor 2
+		operands[1][1][1] = POSITIVE;
+		operands[1][2][0] = 1;              // positive correction of motor 1
+		operands[1][2][1] = POSITIVE;
+		operands[1][3][0] = 3;              // positive correction of motor 3
+		operands[1][3][1] = POSITIVE;
 	} else if (TYPE_CORRECTION == HEADING) {
 		*operands[0] = 4;
 		operands[1] = malloc((sizeof(unsigned int) + sizeof(int)) * *operands[0]);
-		operands[1][0][0] = 0;  // motor
-		operands[1][0][1] = POSITIVE; // arbitrary
-		operands[1][1][0] = 2;  // opposite motor
-		operands[1][1][1] = POSITIVE; // important that operands[1][0][1] is inverse
-		operands[1][2][0] = 1;  // opposite motor
-		operands[1][2][1] = NEGATIVE; // important that operands[1][0][1] is inverse
-		operands[1][3][0] = 3;  // opposite motor
-		operands[1][3][1] = NEGATIVE; // important that operands[1][0][1] is inverse
-	} else if (TYPE_CORRECTION == ALTITUDE) {
-		*operands[0] = 4;
-		operands[1] = malloc((sizeof(unsigned int) + sizeof(int)) * *operands[0]);
-		operands[1][0][0] = 0;  // motor
-		operands[1][0][1] = POSITIVE; // arbitrary
-		operands[1][1][0] = 2;  // opposite motor
-		operands[1][1][1] = POSITIVE; // important that operands[1][0][1] is the same
-		operands[1][2][0] = 1;  // motor
-		operands[1][2][1] = POSITIVE; // important that operands[1][0][1] is the same
-		operands[1][3][0] = 3;  // opposite motor
-		operands[1][3][1] = POSITIVE; // important that operands[1][0][1] is inverse
+		operands[1][0][0] = 0;              // positive correction of motor 0
+		operands[1][0][1] = POSITIVE;
+		operands[1][1][0] = 2;              // positive correction of motor 2
+		operands[1][1][1] = POSITIVE;
+		operands[1][2][0] = 1;              // negative correction of motor 1
+		operands[1][2][1] = NEGATIVE;
+		operands[1][3][0] = 3;              // negative correction of motor 3
+		operands[1][3][1] = NEGATIVE;
 	}
  	return operands;
 }
 
-void correct(const int TYPE_CORRECTION, const int** operands)
+/*******************************************************************************/
+// Main functionality of Flight Control:
+// checks desired DOF against desired values in Nav and corrects PWMDTY of
+// appropriate ESC Inputs.
+/*******************************************************************************/
+void fc_correct(const int TYPE_CORRECTION, const int** operands)
 {
-	int i, adjustment_value,
-	    theta = angle_off(TYPE_CORRECTION);
+	int i, adjustment_value;
+	float theta;
 
-	if (satisfactory(theta))
+	// get Degrees off in Pitch, Roll etc.
+    theta = fc_angle_off(TYPE_CORRECTION);
+
+    // do nothing if nominal
+	if (fc_isNominal(TYPE_CORRECTION, theta))
 		return;
 
 	for (i = 0;  i < *operands[0]; ++i) {
-		if (theta > threshold)
-			adjustment_value = get_adjustment(operands[1][i][0]) / 2;
-		else if (theta < threshold)
-			adjustment_value = get_adjustment(operands[1][i][0]) * 2;
-		else
-			return;
 
-		adjustment_value = (MAX_VAL/adjustment_value) + get_register_value(operands[1][i][0]);
+	    // adjustment proportional to theta * GAIN
+	    adjustment_value =  (theta * fc_gainOf(TYPE_CORRECTION)) * operands[1][i][1];
 
-		adjust(operands[1][i][0], adjustment_value);
+        // Over/Under throttle protection
+        if((adjustment_value + fc_getCurrentPWMDTY(operands[1][i][0]) > MAXDTY) || (adjustment_value + fc_getCurrentPWMDTY(operands[1][i][0]) < MINDTY))
+            adjustment_value = 0;
 
+        // adjust DutyFactor of given channel accordingly
+		fc_adjustDTY(operands[1][i][0], adjustment_value);
 	}
 }
 
-
-void adjust(int register_index, int value)
+/*******************************************************************************/
+// Adjusts the PWMDTY of given channel by given value
+// (each increment of "value" corresponds to a 0.5% increase in throttle
+/*******************************************************************************/
+void fc_adjustDTY(int pwmChannel, int value)
 {
 	if (operands[1][i][0] == 0) {
 		PWMDTY0 += value / 512;
 		PWMDTY1 += value % 512;
 	} else if (operands[1][i][0] == 1) {
-		PWMDTY2 += value/512;
+		PWMDTY2 += value /512;
 		PWMDTY3 += value % 512;
 	} else if (operands[1][i][0] == 2) {
-		PWMDTY4 += value/512;
+		PWMDTY4 += value / 512;
 		PWMDTY5 += value % 512;
 	} else if (operands[1][i][0] == 3) {
-		PWMDTY6 += value/512;
+		PWMDTY6 += value / 512;
 		PWMDTY7 += value % 512;
 	}
+}
+
+/*******************************************************************************/
+// Returns true of desired angle is within appropriate values, false otherwise
+/*******************************************************************************/
+int fc_isNominal(const int TYPE_CORRECTION, float theta)
+{
+    if(TYPE_CORRECTION == 0)
+    {
+        if(theta > PITCHTHRESH || theta < -PITCHTHRESH)
+            return 0;
+        else
+            return 1;
+    }
+    else if(TYPE_CORRECTION == 1)
+    {
+        if(theta > ROLLTHRESH || theta < -ROLLTHRESH)
+            return 0;
+        else
+            return 1;
+    }
+    else if(TYPE_CORRECTION == 2)
+    {
+        if(theta > ALTTHRESH || theta < -ALTTHRESH)
+            return 0;
+        else
+            return 1;
+    }
+    else if(TYPE_CORRECTION == 3)
+    {
+        if(theta > HEADINGTHRESH || theta < -HEADINGTHRESH)
+            return 0;
+        else
+            return 1;
+    }
+}
+
+/*******************************************************************************/
+// Returns difference in desired readings within Nav.
+// (computed by nav_get<parameter>diff)
+/*******************************************************************************/
+float fc_angle_off(const int TYPE_CORRECTION)
+{
+    if(TYPE_CORRECTION == 0)
+        return nav_pitchDiff;
+    else if(TYPE_CORRECTION == 1)
+        return nav_rollDiff;
+    else if(TYPE_CORRECTION == 2)
+        return nav_altitudeDiff;
+    else if(TYPE_CORRECTIOn == 3)
+        return nav_headingDiff;
+}
+
+/*******************************************************************************/
+// Returns current DTY of a given motor
+/*******************************************************************************/
+long fc_getCurrentPWMDTY(int pwmChannel)
+{
+    if(pwmChannel == 0)
+        return (PWMDTY0 << 8 | PWMDTY1);
+    else if(pwmChannel == 1)
+        return (PWMDTY2 << 8 | PWMDTY3);
+    else if(pwmChannel == 2)
+        return (PWMDTY4 << 8 | PWMDTY5);
+    else if(pwmChannel == 3)
+        return (PWMDTY6 << 8 | PWMDTY7);
+}
+
+/*******************************************************************************/
+// Returns the appropriate gain value of a given type of correction
+/*******************************************************************************/
+float fc_gainOf(const int TYPE_CORRECTION)
+{
+    if(TYPE_CORRECTION == 0)
+        return PITCHGAIN;
+    else if(TYPE_CORRECTION == 1)
+        return ROLLLGAIN;
+    else if(TYPE_CORRECTION == 2)
+        return ALTITUDEGAIN;
+    else if(TYPE_CORRECTIOn == 3)
+        return HEADINGGAIM;
 }
--- flightControl.h
+++ flightControl.h
@@ -7,55 +7,19 @@
 #ifndef FLIGHTCONTROL_H_
 #define FLIGHTCONTROL_H_
 
-#include "bmp.h"
-#include "accel.h"
-#include "mag.h"
+/*******************************************************************************/
+// First three must be done in order
+/*******************************************************************************/
+void fc_initialize_motors(void);
+int** fc_get_motors(const int);
+void fc_correct(const int, const int**);
 
-#define ALTTHRESH .4		// meters
-#define HEADINGTHRESH 2.0	//degrees
-#define PITCHTHRESH 2.0		// degrees
-#define ROLLTHRESH 2.0		// degrees
-
-#define COMP_COEF 5
-#define BR 0x1F                 // 100kHz BR
-
-// Flight Control Variables
-/******************************************************************************/
-extern float fc_actualPitch;
-extern float fc_desiredPitch;
-
-extern float fc_actualRoll;
-extern float fc_desiredRoll;
-
-extern float fc_actualAltitude;
-extern float fc_desiredAltitude;
-
-extern float fc_actualHeading;
-extern float fc_desiredHeading;
+void fc_adjustDTY(int, int );
+int fc_isNominal(const int, float );
+float fc_angle_off(const int);
+long fc_getCurrentPWMDTY(int);
+float fc_gainOf(const int);
 
 
-// Flight Control Functions
-/******************************************************************************/
-void fc_setup(void);                       // sets up all sensors and initializes navigation
-
-//Mid Level Utility
-/****************************************************/
-int fc_isHoldingPitch(void);            // compares actual and desired pitches
-int fc_isHoldingRoll(void);				// compares actual and desired rolls
-int fc_isHoldingAltitude(void);         // comapres actual and desired altitudes
-int fc_isHoldingHeading(void);			// compares actual and desired headings
-
-void fc_update(void);
-//Lower Level Utility
-/*****************************************************/
-void fc_capturePitch(void);                 // updates actual pitch
-void fc_captureRoll(void);					// updates actual roll
-void fc_captureAltitude(void);              // updates actual altitude
-void fc_captureHeading(void);				// updates actual heading
-
-void fc_setPitch(float);
-void fc_setRoll(float);
-void fc_setAltitude(float);
-void fc_setHeading(float);
 
 #endif
